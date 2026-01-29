f4aa8d00a80acd022038c24fa040cf6fe10ad55c
Kevin T Garland <usd872@outlook.com>
2026-01-29 17:51:51 +0800

    Update index-page.js

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - 司南搜索`, 
                         window.location.href
                     );
                 } catch (e) {
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
             try {
                 window.history.replaceState(
                     { search: searchQuery }, 
-                    `搜索: ${searchQuery} - LibreTV`, 
+                    `搜索: ${searchQuery} - 司南搜索`, 
                     `/s=${encodeURIComponent(searchQuery)}`
                 );
             } catch (e) {
