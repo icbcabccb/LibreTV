0ae9902d2f4623f8f5600c63a364aaa4adb722b0
Kevin T Garland <usd872@outlook.com>
2026-01-29 21:49:59 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,9 +13,9 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    name: '司南搜索',
+    url: 'https://200805.xyz',
+    description: '免费在线影视搜索与观看平台',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
